a0433cc15d1a05c5fec97ad2bf6a21091163b06b
albertdog <sakhalsa13@gmail.com>
2017-06-24 15:43:06 -0700

    Initial commit

---
+++ LO/script1.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread("LO/galaxy.jpg",0) #adding LO/ changed the type from NONE to numpy.ndarray (don't know why)
+
+print(type(img))
+#print(img)
+#print(img.shape)
+#print(img.ndim)
+
+#resized_image = cv2.resize(img,(1000,500))
+cv2.imshow("Galaxy",img)
+#cv2.waitKey()
+#cv2.destroyAllWindows()
+#print (cv2.__version__)
